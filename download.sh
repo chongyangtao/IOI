c67540aeafeb9d0f71145c1a64b590b200474ef9
CarlosTao <chongyangtao@gmail.com>
2019-09-19 16:18:03 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,10 +2,13 @@
 
 PWD=$(pwd)
 
-UBUNTU_DIR=$PWD/data/ubuntu
+UBUNTU_DIR=$PWD/data/ubuntu1
 mkdir -p $UBUNTU_DIR
 
 # Download Ubuntu dialogue corpus
 wget https://www.dropbox.com/s/2fdn26rj6h9bpvl/ubuntu_data.zip?dl=1 -O $UBUNTU_DIR/ubuntu_data.zip
 unzip -j $UBUNTU_DIR/ubuntu_data.zip -d $UBUNTU_DIR
 
+# Download pre-trained word embedding file
+wget https://www.dropbox.com/s/0ihosy699ga3nie/ubuntu.200d.word2vec.zip?dl=1 -O $UBUNTU_DIR/ubuntu.200d.word2vec.zip
+unzip -j $UBUNTU_DIR/ubuntu.200d.word2vec.zip -d $UBUNTU_DIR
